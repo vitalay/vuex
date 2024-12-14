1d0ea97ba7342df92b8f9b01f2314f7bbfe89d0f
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-14 18:15:05 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid ">
+  <h1>App</h1>
+
+  <TheCouterVue />
+  </div>
+</template>
+
+<script>
+
+import TheCouterVue from "./TheCouter.vue";
+
+export default {
+
+  components: {
+    TheCouterVue,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/TheCouter.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Counter</h1>
+  <h2>{{ $store.state.count }}</h2>
+  <button class="btn btn-primary" @click="increase">Увеличить</button>
+</template>
+
+<script>
+export default {
+    methods: {
+        increase() {
+            this.$store.state.count++
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
