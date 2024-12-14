aa7e2b2b4e8354cb369f28c75426f4ae2d8540a1
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-14 19:08:20 +0500

    computed  улучшение кода

--- src/TheCouter.vue
+++ src/TheCouter.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Counter</h1>
-  <h2>{{ $store.getters.count }}</h2>
+  <h2>{{ count }}</h2>
   <button class="btn btn-primary" @click="increase">Увеличить</button>
   <h2>Умноженое на 3</h2>
   <h2>{{ $store.getters.multBythree }}</h2>
@@ -10,6 +10,11 @@
 
 <script>
 export default {
+    computed: {
+        count() {
+            return this.$store.getters.count
+        }
+    },
     methods: {
         increase() {
             this.$store.commit('increase', 10)
